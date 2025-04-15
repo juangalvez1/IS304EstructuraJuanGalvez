8ac46b470eccec08a16a36b0e0531a4b50dc2e5f
juangalvez1 <juan.galvez1@utp.edu.co>
2025-04-15 00:29:05 -0500

    Creation of getter/setter methods to the Node class, and making its atributes private

--- 5.RedBlackTree/Estructura_RBT.cc
+++ 5.RedBlackTree/Estructura_RBT.cc
@@ -8,73 +8,89 @@ template<typename Type>
 class RBT{
     private:
         class Node{
+            private:
+                Type data_;
+                bool color_;
+                Node* left_;
+                Node* right_;
+                Node* parent_;
+
             public:
-                Type data;
-                bool color;
-                Node* left;
-                Node* right;
-                Node* parent;
+                Node(Type elem = Type()) : data_(elem), left_(nullptr), right_(nullptr), parent_(nullptr), color_(RED) {}
+                
+                Type& data(){
+                    return data_;
+                }
 
-                Node(Type elem = Type()) : data(elem), left(nullptr), right(nullptr), parent(nullptr), color(RED) {}
+                bool& color(){
+                    return color_;
+                }
 
-                // Type get_data(){
-                //     return data;
-                // }
+                Node*& left(){
+                    return left_;
+                }
 
-                // bool get_color(){
-                //     return color;
-                // }
+                Node*& right(){
+                    return right_;
+                }
+
+                Node*& parent(){
+                    return parent_;
+                }
         };
 
         Node* root_;
 
         void rotateLeft(Node *x){
-            Node *y = x->right;
-            x->right = y->left;
+            Node *y = x->right();
+            x->right() = y->left();
             
-            if(y->left != nullptr)
-                y->left->parent = x;
+            if(y->left() != nullptr)
+                y->left()->parent() = x;
             
-            y->parent = x->parent;
+            y->parent() = x->parent();
 
-            if(x->parent == nullptr){
+            if(x->parent() == nullptr){
                 root_ = y;
-            } else if(x == x->parent->left){ // Verificamos si x es un hijo izquierdo
-                x->parent->left = y;
+            } else if(x == x->parent()->left()){ // Verificamos si x es un hijo izquierdo
+                x->parent()->left() = y;
             } else{
-                x->parent->right = y;
+                x->parent()->right() = y;
             }
         
-            y->left = x;
-            x->parent = y;
+            y->left() = x;
+            x->parent() = y;
         }
 
-        void rotateRight(Node* y){
-            Node* x = y->left;
-            y->left = x->right;
+        void rotateRight(Node *y){
+            Node* x = y->left();
+            y->left() = x->right();
 
-            if(x->right != nullptr)
-                x->right->parent = y;
+            if(x->right() != nullptr)
+                x->right()->parent() = y;
 
-            x->parent = y->parent;
+            x->parent() = y->parent();
 
-            if(y->parent == nullptr){
+            if(y->parent() == nullptr){
                 root_ = x;
-            } else if(y == y->parent->left){ // Verificamos si y es un hijo izquierdo
-                y->parent->left = x;
+            } else if(y == y->parent()->left()){ // Verificamos si y es un hijo izquierdo
+                y->parent()->left() = x;
             } else {
-                y->parent->right = x;
+                y->parent()->right() = x;
             }
 
-            x->right = y;
-            y->parent = x;
+            x->right() = y;
+            y->parent() = x;
         }
 
     public:
         RBT() : root_(nullptr) {}
 
-        void insert(Type elem){
-
+        void insert(Type& elem, Node* start= nullptr){
+            if (start == nullptr) {
+                start = root_;
+            }
+            Node* newNode = new Node(elem);
         }
 };
 
