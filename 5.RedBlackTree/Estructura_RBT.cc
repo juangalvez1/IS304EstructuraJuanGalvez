3bc562e2e95d378f2a75b14527cc2653ab37b60a
juangalvez1 <juan.galvez1@utp.edu.co>
2025-04-20 23:38:51 -0500

    Implementation of insert and size methods on RBT datastructure

--- 5.RedBlackTree/Estructura_RBT.cc
+++ 5.RedBlackTree/Estructura_RBT.cc
@@ -40,6 +40,7 @@ class RBT{
         };
 
         Node* root_;
+        unsigned int size_;
 
         void rotateLeft(Node *x){
             Node *y = x->right();
@@ -83,14 +84,118 @@ class RBT{
             y->parent() = x;
         }
 
-    public:
-        RBT() : root_(nullptr) {}
+        void fixInsert(Node *node){
+            while(node != root_ && node->parent()->color() == RED){
+                Node* parent = node->parent();
+                Node* grandParent = parent->parent();
 
-        void insert(Type& elem, Node* start= nullptr){
-            if (start == nullptr) {
-                start = root_;
+                if(parent == grandParent->left()){
+                    Node* uncle = grandParent->right();
+
+                    if(uncle != nullptr && uncle->color() == RED){
+
+                        parent->color() = BLACK;
+                        uncle->color() = BLACK;
+                        grandParent->color() = RED;
+                        node = grandParent;
+
+                    } else {
+                        // RI case
+                        if(node == parent->right()){
+                            node = parent;
+                            rotateLeft(node);
+                        }
+                        // II case
+                        parent->color() = BLACK;
+                        grandParent->color() = RED;
+                        rotateRight(grandParent);
+
+                    }
+                } else {
+                    Node* uncle = grandParent->left();
+
+                    if(uncle != nullptr && uncle->color() == RED){
+
+                        parent->color() = BLACK;
+                        uncle->color() = BLACK;
+                        grandParent->color() = RED;
+                        node = grandParent;
+
+                    } else {
+                        // IR case
+                        if(node == parent->left()){
+                            node = parent;
+                            rotateRight(node);
+                        }
+                        // RR case
+                        parent->color() = BLACK;
+                        grandParent->color() = RED;
+                        rotateLeft(grandParent);
+
+                    }
+                }
             }
+            root_->color() = BLACK;
+        }
+
+    public:
+        RBT() : root_(nullptr), size_(0) {}
+
+        void insert(const Type& elem){
             Node* newNode = new Node(elem);
+            if (root_ == nullptr) {
+                newNode->color() = BLACK;
+                root_ = newNode;
+                size_++;
+                return;
+            }
+
+            Node* curr = root_;
+            Node* parent = nullptr;
+
+            while (curr != nullptr) {
+                assert(elem != curr->get_data());
+                parent = curr;
+                if(elem < curr->data()){
+                    curr = curr->left();
+                } else {
+                    curr = curr->right();
+                }
+            }
+
+            newNode->parent() = parent;
+
+            if(elem < parent->data()){
+                parent->left() = newNode;
+            } else {
+                parent->right() = newNode;
+            }
+
+            size_++;
+
+            if(parent->color() == RED){
+                fixInsert(newNode);
+            }
+        }
+
+        bool find(Type& elem) const {
+            Node* curr = root_;
+
+            while (curr != nullptr) {
+                if (elem == curr->data()) {
+                    return true;
+                } else if (elem < curr->data()) {
+                    curr = curr->left();
+                } else {
+                    curr = curr->right();
+                }
+            }
+            
+            return false;
+        }
+
+        unsigned int size() const {
+            return size_;
         }
 };
 
